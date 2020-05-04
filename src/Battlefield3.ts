ee2842fff01abb92d96efe09646c807c243add17
multivit4min <david.kartnaller@gmail.com>
2020-05-04 18:56:29 +0000

    implement last missing commands

--- src/Battlefield3.ts
+++ src/Battlefield3.ts
@@ -200,10 +200,20 @@ export class Battlefield3 extends EventEmitter {
       .send()
   }
 
+  /** get the battlefield server salt */
   private getSalt() {
     return this.rcon.createCommand<Buffer>("login.hashed")
       .format(w => Buffer.from(w[0].toString(), "hex"))
       .send()
+  }
+
+  /** creates the hashed password from the actual password and the hash */
+  private getPasswordHash(password: string, hash: Buffer) {
+    return createHash("md5")
+      .update(hash)
+      .update(password)
+      .digest("hex")
+      .toUpperCase()
   }
 
   /**
@@ -211,12 +221,7 @@ export class Battlefield3 extends EventEmitter {
    * @param password password to login with
    */
   async login(password: string) {
-    const hash = createHash("md5")
-      .update(await this.getSalt())
-      .update(password)
-      .digest("hex")
-      .toUpperCase()
-    return this.rcon.createCommand("login.hashed", hash).send()
+    return this.rcon.createCommand("login.hashed", this.getPasswordHash(password, await this.getSalt())).send()
   }
 
   /**
@@ -484,6 +489,142 @@ export class Battlefield3 extends EventEmitter {
   }
 
   /**
+   * clears the map list and loads it from disk again
+   */
+  loadMapList() {
+    return this.rcon.createCommand("mapList.load").send()
+  }
+
+  /**
+   * saves the maplist to disk
+   */
+  saveMapList() {
+    return this.rcon.createCommand("mapList.save").send()
+  }
+
+  /**
+   * Adds the map <map>, with gamemode <gamemode>, for <rounds> rounds, to the
+   * maplist. If <index> is not specified, it is appended to the end; otherwise, it is inserted
+   * before the map which is currently at position <index>.
+   * @param map 
+   * @param mode 
+   * @param rounds 
+   * @param index 
+   */
+  addMap(map: string, mode: string, rounds: number = 2, index?: number) {
+    return this.rcon.createCommand("mapList.add", map, mode, rounds, index).send()
+  }
+
+  /**
+   * Removes the map at offset <index> from the maplist
+   * @param index 
+   */
+  removeMap(index: number) {
+    return this.rcon.createCommand("mapList.remove", index).send()
+  }
+
+  /**
+   * clears the map list.
+   */
+  clearMapList() {
+    return this.rcon.createCommand("mapList.clear").send()
+  }
+
+  /**
+   * Returns a section of the map list.
+   * At most 100 entries will be returned by the command.
+   * To retrieve the full list, perform several mapList.list calls with increasing offset until the
+   * server returns 0 entries.
+   * (There is an unsolved synchronization problem hidden there: if the map list is edited by another
+   * RCON client during this process, then entries may be missed during retrieval. There is no
+   * known workaround for this.)
+   * @param offset 
+   */
+  getMaps(offset?: number) {
+    return this.rcon.createCommand<Battlefield3.MapList>("mapList.list", offset)
+      .format(w => {
+        return w.slice(2).reduce((acc, _, i, arr) => {
+          if (i % 3 !== 0) return acc
+          return [...acc, {
+            map: arr[i].toString(),
+            mode: arr[i+1].toString(),
+            rounds: arr[i+2].toNumber(),
+            index: (offset||0) + i / 3 + 1
+          }]
+        }, [] as Battlefield3.MapList)
+      })
+      .send()
+  }
+
+  /**
+   * Specifies which map to switch to once the current round completes. If there are rounds remaining
+   * on the current map, those rounds will be skipped.
+   * @param index 
+   */
+  setNextMapIndex(index: number) {
+    return this.rcon.createCommand("mapList.setNextMapIndex", index).send()
+  }
+
+  /**
+   * returns the index of the map that is currently being played, and the index of the next map to run.
+   */
+  getMapIndices() {
+    return this.rcon.createCommand<{ index: number, next: number }>("mapList.getMapIndices")
+      .format(w => ({ index: w[0].toNumber(), next: w[1].toNumber() }))
+      .send()
+  }
+
+  /**
+   * switches immediately to the next round, without going through the end-of-round sequence.
+   */
+  nextRound() {
+    return this.rcon.createCommand("mapList.runNextRound").send()
+  }
+
+  /**
+   * dnd the current round, declaring <winner> as the winning team
+   */
+  endRound(winner: number) {
+    return this.rcon.createCommand("mapList.endRound", winner).send()
+  }
+
+  /**
+   * returns the (1-based) current round number, and total number of rounds before switching map.
+   */
+  getRounds() {
+    return this.rcon.createCommand<{ current: number, total: number }>("mapList.getRounds")
+      .format(w => ({ current: w[0].toNumber(), total: w[1].toNumber() }))
+      .send()
+  }
+
+  /**
+   * updates a set of variables
+   * @param vars 
+   */
+  setVariables(vars: Battlefield3.Variables) {
+    return Promise.all(
+      //@ts-ignore
+      Object.keys(vars).map(k => this.setVariable(k, vars[k]))
+    )
+  }
+
+  /** gets a set of variables */
+  getVariables(vars: (keyof Battlefield3.Variables)[]): Promise<Record<keyof Battlefield3.Variables, string>> {
+    return Promise.all(vars.map(v => [v, this.getVariable(v)])).then(res => Object.fromEntries(res))
+  }
+
+  getVariable(name: keyof Battlefield3.Variables) {
+    return this.rcon.createCommand<string>(`vars.${(name.startsWith("_")) ? name.substr(1) : name}`)
+      .format(w => w[0].toString())
+      .send()
+  }
+
+  setVariable(name: keyof Battlefield3.Variables, value: boolean|number|string) {
+    return this.rcon.createCommand(`vars.${(name.startsWith("_")) ? name.substr(1) : name}`, value)
+      .send()
+  }
+
+  /**
    * creates a new timeout instance
    * @param type duration
    */
@@ -555,6 +696,15 @@ export namespace Battlefield3 {
 
   export type ListPlayer = Player.Info[]
 
+  export type MapList = MapEntry[]
+
+  export interface MapEntry {
+    map: string
+    mode: string
+    rounds: number
+    index: number
+  }
+
   export interface ServerInfo {
     name: string
     slots: number
@@ -573,5 +723,87 @@ export namespace Battlefield3 {
     address: string
     punkBusterVersion: string
     joinQueueEnabled: boolean
+  }
+
+  
+
+  export interface Variables {
+    /* This command can only be used during startup. It can only be used to switch the server from ranked to unranked mode; the server can never switch back to ranked mode again. */
+    ranked: boolean
+    /* Set server name */
+    serverName: string
+    /* Set the game password for the server, use it with an empty string to reset */
+    gamePassword: string
+    /* Set if the server should autobalance */
+    autoBalance: boolean
+    /* Set if the server should allow team damage */
+    friendlyFire: boolean
+    /* Set desired maximum number of players */
+    maxPlayers: number
+    /* Sets the server description. This string is displayed on the server’s detail page on Battlelog.
+     * This string must be less than 256 characters in length. */
+    serverDescription: string
+    /* Sets the server welcome message. This message will be displayed via an admin.yell to each player the first time that player deploys in on the server.
+     * The message is displayed for 5 seconds. This string must be less than 256 characters in length. */
+    serverMessage: string
+    /* Set if killcam is enabled */
+    killCam: boolean
+    /* Set if minimap is enabled */
+    miniMap: boolean
+    /* Set if players hud is available */
+    hud: boolean
+    /* Set if crosshair for all weapons is enabled */
+    crossHair: boolean
+    /* Set if spotted targets are visible in the 3d-world */
+    _3dSpotting: boolean
+    /* Set if spotted targets are visible on the minimap */
+    miniMapSpotting: boolean
+    /* Set if nametags should be displayed */
+    nametag: boolean
+    /* Set if players should be allowed to switch to third-person vehicle cameras */
+    _3pCam: boolean
+    /* Set if players health regeneration is active */
+    regenerateHealth: boolean
+    /* Set number of teamkills allowed during one round, before the game kicks the player in question Set to 0 to disable kill counting */
+    teamKillCountForKick: number
+    /* Set the highest kill-value allowed before a player is kicked for teamkilling Set to 0 to disable kill value mechanism */
+    teamKillValueForKick: number
+    /* Set the value of a teamkill (adds to the player’s current kill-value) */
+    teamKillValueIncrease: number
+    /* Set how much every player’s kill-value should decrease per second */
+    teamKillValueDecreasePerSecond: number
+    /* Set how many teamkill-kicks will lead to permaban Set to 0 to disable feature */
+    teamKillKickForBan: number
+    /* Set how many seconds a player can be idle before he/she is kicked from server Set to 0 to disable idle kick */
+    idleTimeout: number
+    /* Set how many rounds an idle-kick person should be banned Set to 0 to disable ban mechanism */
+    idleBanRounds: number
+    /* Set the minimum number of players required to begin a round */
+    roundStartPlayerCount: number
+    /* Set the minimum number of players for the round to restart in pre-round */
+    roundRestartPlayerCount: number
+    /* Set the duration of pre-round */
+    roundLockdownCountdown: number
+    /* Set whether vehicles should spawn in-game */
+    vehicleSpawnAllowed: number
+    /* Set vehicle spawn delay scale factor, in percent */
+    vehicleSpawnDelay: number
+    /* Set soldier max health scale factor, in percent */
+    soldierHealth: number
+    /* Set player respawn time scale factor, in percent */
+    playerRespawnTime: number
+    /* Set player man-down time scale factor, in percent */
+    playerManDownTime: number
+    /* Set bullet damage scale factor, in percent */
+    bulletDamage: number
+    /* Set scale factor for number of tickets to end round, in percent */
+    gameModeCounter: number
+    /* Set if players can only spawn on their squad leader */
+    onlySquadLeaderSpawn: boolean
+    /* Set which group of weapons/unlock should be available to players on an unranked server */
+    //@todo specification
+    unlockMode: string
+    /* Set if server should be exclusive to Premium players */
+    premiumStatus: boolean
   }
 }
