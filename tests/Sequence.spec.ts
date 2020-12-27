3227d11590fb4384073cd4ee6b47881145b0c259
multivit4min <david.kartnaller@gmail.com>
2020-12-27 16:42:41 +0000

    fix tests

--- tests/Sequence.spec.ts
+++ tests/Sequence.spec.ts
@@ -7,7 +7,7 @@ describe("Sequence", () => {
     it("should validate a buffer", () => {
       const seq = Sequence.from(Buffer.alloc(4))
       expect(seq.origin).toBe(Sequence.Origin.SERVER)
-      expect(seq.type).toBe(Sequence.Type.REQUEST)
+      expect(seq.type).toBe(Sequence.Type.RESPONSE)
       expect(seq.sequence).toBe(0)
     })
 
@@ -21,7 +21,7 @@ describe("Sequence", () => {
       buffer.writeUInt32LE(0x80000000, 0)
       const seq = Sequence.from(buffer)
       expect(seq.origin).toBe(Sequence.Origin.SERVER)
-      expect(seq.type).toBe(Sequence.Type.REQUEST)
+      expect(seq.type).toBe(Sequence.Type.RESPONSE)
       expect(seq.sequence).toBe(0)
     })
 
@@ -35,7 +35,7 @@ describe("Sequence", () => {
       buffer.writeUInt32LE(0x40000000, 0)
       const seq = Sequence.from(buffer)
       expect(seq.origin).toBe(Sequence.Origin.SERVER)
-      expect(seq.type).toBe(Sequence.Type.RESPONSE)
+      expect(seq.type).toBe(Sequence.Type.REQUEST)
       expect(seq.sequence).toBe(0)
     })
 
@@ -49,7 +49,7 @@ describe("Sequence", () => {
       buffer.writeUInt32LE(0x20538003, 0)
       const seq = Sequence.from(buffer)
       expect(seq.origin).toBe(Sequence.Origin.SERVER)
-      expect(seq.type).toBe(Sequence.Type.REQUEST)
+      expect(seq.type).toBe(Sequence.Type.RESPONSE)
       expect(seq.sequence).toBe(542343171)
     })
 
@@ -63,11 +63,11 @@ describe("Sequence", () => {
       buffer.writeUInt32LE(0xC0000000, 0)
       let seq = Sequence.from(buffer)
       expect(seq.origin).toBe(Sequence.Origin.SERVER)
-      expect(seq.type).toBe(Sequence.Type.RESPONSE)
-      expect(seq.sequence).toBe(0)
-      seq = seq.nextSequence({ origin: Sequence.Origin.CLIENT, type: Sequence.Type.REQUEST })
-      expect(seq.origin).toBe(Sequence.Origin.CLIENT)
       expect(seq.type).toBe(Sequence.Type.REQUEST)
+      expect(seq.sequence).toBe(0)
+      seq = seq.nextSequence({ origin: Sequence.Origin.CLIENT, type: Sequence.Type.RESPONSE })
+      expect(seq.origin).toBe(Sequence.Origin.CLIENT)
+      expect(seq.type).toBe(Sequence.Type.RESPONSE)
       expect(seq.sequence).toBe(1)
     })
 
